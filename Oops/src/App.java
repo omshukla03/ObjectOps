6b641329c9c3f48aa087bd412779b76635360dc1
Om Shukla <work.omshukla@gmail.com>
2024-10-02 21:03:13 +0530

    Adding Methods And Polymorphism

--- Oops/src/App.java
+++ Oops/src/App.java
@@ -1,5 +1,36 @@
+
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+         Person p1=new Person();
+         p1.age=20;
+         p1.name="Om";
+    
+         Person  p2=new Person();
+         p2.age=30;
+         p2.name="Shukla";
+       
+        // Calling eat & walk methods(Functions) in p1  
+        p1.eat();
+        p2.walk();
+        p2.walk(10);
+ 
+     }
+ }
+
+ class Person{
+     String name;
+     int age;
+
+    void walk(){
+        System.out.println(name + " is Walking");
     }
-}
+    void eat(){
+        System.out.println(name + " is Eating");
+    }
+    void walk(int steps){
+        System.out.println(name + " is Walking " + steps + " steps");
+    }
+ }

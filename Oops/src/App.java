f679d4ccc30fb571f7f77c4ed1e35d40b9bb1bdc
Om Shukla <work.omshukla@gmail.com>
2024-10-02 20:43:52 +0530

    Explain Classes and Objects

--- Oops/src/App.java
+++ Oops/src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Create an Object(P1,P2) of Class Person
+        Person p1=new Person();
+        p1.age=20;
+        p1.name="Om";
+        System.out.println(p1.age+" "+p1.name);
+
+        Person  p2=new Person();
+        p2.age=30;
+        p2.name="Shukla";
+        System.out.println(p2.age+" "+p2.name);
+
+
     }
+}
+//Create a Class Named Person
+class Person{
+    String name;
+    int age;
 }
